65dbc4bdc1fc0c4beaf4c12101d3204f4000be09
gadasilvas <gabriel.seles@engdb.com.br>
2021-03-10 16:43:44 -0300

    Cancelando Tasks

--- UsingAsyncTasks/Program.cs
+++ UsingAsyncTasks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UsingAsyncTasks
@@ -8,8 +9,31 @@ namespace UsingAsyncTasks
     {
         static async Task Main(string[] args)
         {
+            //Utilizando o WhenAll - O WhenAll serve para executar em segundo plano, todos os métodos em paralelo, e chamar ele quando a execução do anterior estiver ok, 
+            //diminuindo o tempo total de carregamento
             //await ExecutaTarefaAsync();
-            await ExecutaTarefasAsyncWhenAll();
+            //await ExecutaTarefasAsyncWhenAll();
+
+
+            //Cancelando operações assincronas
+
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                //Console.WriteLine("Cancelamento automático após 1.5segundos");
+                //await ExecutaCancelamento(2000);
+                Console.WriteLine("Cancelamento apos tecla");
+                await ExecutaCancelamentoAposTeclar();
+            }
+            catch (Exception)
+            {
+
+                Console.Write($"Tarefa cancelada após {stopwatch.Elapsed}");
+            }
+            Console.ReadKey();
+
         }
 
         private static async Task ExecutaTarefasAsyncWhenAll()
@@ -24,6 +48,9 @@ namespace UsingAsyncTasks
             Console.WriteLine("Tempo gasto para extrair e inserir dados.. " + stopwatch.Elapsed);
             Console.WriteLine("Resultado de extrair dados = " + dadosTask.Result);
             Console.WriteLine("Resultado de inserir dados = " + resultadoTask.Result);
+
+
+
         }
 
 
@@ -53,6 +80,75 @@ namespace UsingAsyncTasks
             await Task.Delay(TimeSpan.FromSeconds(3));
             return resultado;
         }
+
+
+        //Cancelamento de operação
+        public static async Task ExecutaCancelamento(int tempo)
+        {
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                cancellationTokenSource.CancelAfter(tempo);
+                try
+                {
+                    var resultado = await OperacaoLongauracaoCancelavel(100, cancellationTokenSource.Token);
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+                catch (TaskCanceledException)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+
+        public static async Task ExecutaCancelamentoAposTeclar()
+        {
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                var tecladoTask = Task.Run(()=>
+                {
+                    Console.WriteLine("Pressione uma tecla para cancelar");
+                    Console.ReadKey();
+                    cancellationTokenSource.Cancel();
+                });
+
+                try
+                {
+                    var tarefa = OperacaoLongauracaoCancelavel(500, cancellationTokenSource.Token);
+                    var resultado = await tarefa;
+                    Console.WriteLine($"Resultado {resultado}");
+                }
+                catch (TaskCanceledException)
+                {
+
+                    throw;
+                }
+                await tecladoTask;
+            }
+        }
+
+        private static Task<int> OperacaoLongauracaoCancelavel(int valor, CancellationToken cancellationToken = default)
+        {
+            Console.WriteLine("Executou Operacao de Longa duração");
+
+            Task<int> task = null;
+
+            task = Task.Run(() =>
+            {
+                int resultado = 0;
+                for (int i = 0; i < valor; i++)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                        throw new TaskCanceledException(task);
+                    
+
+                    Thread.Sleep(10);
+                    resultado += i;
+                }
+                return resultado;
+            });
+            return task;
+        }
     }
 }
- 
